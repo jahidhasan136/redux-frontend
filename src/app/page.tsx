e75739359d6b0a59666560b5d2b7fd1395765b3c
jahidhasan136 <jh3495689@gmail.com>
2025-01-04 12:15:16 +0600

    counter increment or decrement functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,32 @@
-import Image from "next/image";
+"use client";
+
+import { decrement, increment } from "@/redux/slice/counterSlice";
+import { RootState } from "@/redux/store";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Home() {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
   return (
     <div className="gird place-items-center pt-10">
-      <button className="text-3xl font-bold p-4 border rounded-xl">
+      <button
+        onClick={() => handleIncrement()}
+        className="text-3xl font-bold p-4 border rounded-xl"
+      >
         Increment
       </button>
-      <p className="text-xl py-10">0</p>
-      <button className="text-3xl font-bold p-4 border rounded-xl">
+      <p className="text-xl py-10">{count}</p>
+      <button
+        onClick={() => handleDecrement()}
+        className="text-3xl font-bold p-4 border rounded-xl"
+      >
         Decrement
       </button>
     </div>
---
+++ src/redux/rootReducer.ts
@@ -0,0 +1,5 @@
+import { counterSlice } from "./slice/counterSlice";
+
+export const reducer = {
+  counter: counterSlice.reducer,
+};
